059ed97fa289bf9c2101cbb011906888b55122b4
Dead End <dead.end@inbox.ru>
2024-01-20 11:46:09 +0100

    update crypt

--- src/ts/crypt.ts
+++ src/ts/crypt.ts
@@ -1,67 +1,39 @@
-import { base64ToBytes, bytesToBase64 } from "byte-base64";
+import { arrayBufferToBase64, base64ToArrayBuffer } from "./utils";
 
 /**
- *
- */
-export const doDecrypt = async (
-  key: CryptoKey,
-  iv: Uint8Array,
-  base64: string
-) => {
-  const uint8Array = base64ToBytes(base64);
-  const encArrBuf = uint8Array.buffer;
-
-  const decArrBuf = await window.crypto.subtle.decrypt(
-    {
-      name: "AES-GCM",
-      iv: iv,
-    },
-    key,
-    encArrBuf
-  );
-
-  return new TextDecoder().decode(decArrBuf);
-};
-
-/**
- *
- */
-export const doEncrypt = async (
-  key: CryptoKey,
-  iv: Uint8Array,
-  msg: string
-) => {
-  const encArrBuf = await crypto.subtle.encrypt(
-    {
-      name: "AES-GCM",
-      iv,
-    },
-    key,
-    new TextEncoder().encode(msg)
-  );
-
-  return bytesToBase64(new Uint8Array(encArrBuf));
-};
-
-/**
- *
+ * The function creates random data and stores it in the local storage.
  */
 const getStoredRandomBytes = (key: string, length: number) => {
   const stored = localStorage.getItem(key);
   if (stored) {
-    return base64ToBytes(stored);
+    const arrBuf = base64ToArrayBuffer(stored);
+    return new Uint8Array(arrBuf)
   }
 
   const iv = crypto.getRandomValues(new Uint8Array(length));
-  const base64 = bytesToBase64(iv);
+  const base64 = arrayBufferToBase64(iv.buffer);
   localStorage.setItem(key, base64);
   return iv;
 };
 
 /**
+ * The function returns salt.
+ */
+const getSalt = () => {
+  return getStoredRandomBytes("salt", 12);
+};
+
+/**
+ * The function return an iv as an Uint8Array
+ */
+const getIvU8Arr = () => {
+  return crypto.getRandomValues(new Uint8Array(12));
+}
+
+/**
  * The function creates a derived key from a password.
  */
-export const getKey = async (pwd: string, salt: Uint8Array) => {
+const getKey = async (pwd: string, salt: Uint8Array) => {
   const importKey = await crypto.subtle.importKey(
     "raw",
     new TextEncoder().encode(pwd),
@@ -86,10 +58,71 @@ export const getKey = async (pwd: string, salt: Uint8Array) => {
   return deriveKey;
 };
 
-export const getSalt = () => {
-  return getStoredRandomBytes("salt", 12);
+/**
+ * The function calls the crypto web api to encrypt a string.
+ */
+const rawEncrypt = async (key: CryptoKey, iv: Uint8Array, msg: string) => {
+  const encArrBuf = await crypto.subtle.encrypt(
+    {
+      name: "AES-GCM",
+      iv,
+    },
+    key,
+    new TextEncoder().encode(msg)
+  );
+
+  return encArrBuf;
 };
 
-export const getIv = () => {
-  return getStoredRandomBytes("iv", 12);
+/**
+ * The function calls the crypto web api to decrypt a string.
+ */
+const rawDecrypt = async (key: CryptoKey, iv: Uint8Array, encArrBuf: ArrayBuffer) => {
+
+  const decArrBuf = await window.crypto.subtle.decrypt(
+    {
+      name: "AES-GCM",
+      iv,
+    },
+    key,
+    encArrBuf
+  );
+
+  return new TextDecoder().decode(decArrBuf);
 };
+
+/**
+ * The function encrypts a string with a derived key and returns the result as
+ * a base64 string.
+ */
+export const encrypt = async (data: string, password: string) => {
+  const ivU8Arr = getIvU8Arr();
+
+  const key = await getKey(password, getSalt())
+  const encArrBuf = await rawEncrypt(key, ivU8Arr, data)
+
+  const encBase64 = arrayBufferToBase64(encArrBuf);
+  const ivBase64 = arrayBufferToBase64(ivU8Arr.buffer);
+
+  const encrypted = ivBase64 + '#' + encBase64
+
+  return encrypted
+}
+
+/**
+ * The function is called with a encrypted, base64 string and decrypts it. The
+ * plain text is returned.
+ */
+export const decrypt = async (encrypted: string, password: string) => {
+  const [ivBase64, encBase64] = encrypted.split('#')
+
+  const ivArrBuff = base64ToArrayBuffer(ivBase64)
+  const encArrBuf = base64ToArrayBuffer(encBase64)
+
+  const ivU8Arr = new Uint8Array(ivArrBuff)
+
+  const key = await getKey(password, getSalt())
+  const decrypted = await rawDecrypt(key, ivU8Arr, encArrBuf)
+
+  return decrypted
+}
--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -6,5 +6,19 @@ export const formDataStrValue = (value: FormDataEntryValue | null): string => {
     if (value === null || typeof value !== "string") {
         throw Error(`Invalid value: ${value}`);
     }
-    return value;
+    return value.trim();
 };
+
+/**
+ * The function returns a base64 from an ArrayBuffer.
+ */
+export const arrayBufferToBase64 = (buffer: ArrayBuffer) => {
+    return btoa(String.fromCharCode(...new Uint8Array(buffer)));
+}
+
+/**
+ * The function returns an ArrayBuffer to a base64 string.
+ */
+export const base64ToArrayBuffer = (base64: string) => {
+    return Uint8Array.from(atob(base64), c => c.charCodeAt(0)).buffer
+}
