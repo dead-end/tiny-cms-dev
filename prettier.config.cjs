0960c42cfd0099d1d631d87a941617f140c961f9
Dead End <dead.end@inbox.ru>
2024-01-27 00:32:02 +0100

    not working

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,12 +1,9 @@
 // prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
 
 /** @type {import("prettier").Config} */
-//const config = {
 module.exports = {
     trailingComma: 'none',
     tabWidth: 4,
     semi: false,
     singleQuote: true
 }
-
-//export default config;
