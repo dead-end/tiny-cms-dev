e4629500227b98e0e9bbdbc6ca16c335f11af531
Dead End <dead.end@inbox.ru>
2024-02-04 09:54:25 +0100

    update

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,32 +1,60 @@
 <script lang="ts">
     import {
         gqlGetFile,
+        gqlGetFiles,
         gqlGetListing,
         type TFile,
         type TListing
     } from '../ts/graphql'
+    import {} from '../ts/libs/result'
     import { repoConfigStore } from '../ts/stores/repoConfig'
 
     let listing: TListing[] = []
 
     let file: TFile
+    let files: TFile[]
 
     const query = async () => {
-        listing = await gqlGetListing(
+        const result = await gqlGetListing(
             $repoConfigStore,
             'collections/search-engine'
         )
+
+        if (result.hasError()) {
+            console.log('ERROR', result.getError())
+            return
+        }
+
+        listing = result.getValue()
+    }
+
+    const getFiles = async () => {
+        const result = await gqlGetFiles(
+            $repoConfigStore,
+            ['collections/search-engine/bing.json',
+            'collections/search-engine/google.json']
+        )
+
+        if (result.hasError()) {
+            console.log('ERROR', result.getError())
+            return
+        }
+
+        files = result.getValue()
     }
 
     const getFile = async () => {
-        const a = await gqlGetFile(
+        const result = await gqlGetFile(
             $repoConfigStore,
             'collections/search-engine/bing.json'
         )
 
-        if (a) {
-            file = a
+        if (result.hasError()) {
+            console.log('ERROR', result.getError())
+            return
         }
+
+        file = result.getValue()
     }
 </script>
 
@@ -34,6 +62,7 @@
     <h1>About</h1>
     <button on:click={query} class="btn-base">Query</button>
     <button on:click={getFile} class="btn-base">Bing</button>
+    <button on:click={getFiles} class="btn-base">Bings</button>
 
     {#if listing}
         <ul>
@@ -58,5 +87,12 @@
     {#if file}
         <h4>{file.oid}</h4>
         <p>{file.text}</p>
+    {/if}
+
+    {#if files}
+        {#each files as f}
+            <h4>{f.oid}</h4>
+            <p>{f.text}</p>
+        {/each}
     {/if}
 </div>
--- src/ts/graphql.ts
+++ src/ts/graphql.ts
@@ -1,7 +1,9 @@
+import { resultError, resultSuccess } from './libs/result';
 import type { TRepoConfig } from "./stores/repoConfig";
 
 const GQL_URL = "https://api.github.com/graphql"
 
+// TODO: Correct name ? Correct location?
 export type TListing = {
     name: string;
     type: string;
@@ -13,6 +15,8 @@ export type TFile = {
     oid: string;
 }
 
+
+// TODO: pagination
 const getListingQuery = (owner: string, name: string, branch: string, path: string) => {
     const query = `
     query Listing($owner: String!, $name: String!, $path: String!) {
@@ -38,6 +42,7 @@ const getListingQuery = (owner: string, name: string, branch: string, path: stri
     return { query, variables }
 }
 
+// TODO: Remove in the long run
 const getFileQuery = (owner: string, name: string, branch: string, path: string) => {
     const query = `
     query getFile($owner: String!, $name: String!, $path: String!) {
@@ -60,8 +65,41 @@ const getFileQuery = (owner: string, name: string, branch: string, path: string)
     return { query, variables }
 }
 
+const getFilesQuery = (owner: string, name: string, branch: string, paths: string[]) => {
+    const arr: string[] = []
 
+    arr.push(`
+    query getFile($owner: String!, $name: String!) {
+        repository(owner: $owner, name: $name) {
+    `)
 
+    let count = 0
+
+    paths.forEach(path => {
+        arr.push(`
+        file${count++}: object(expression: "${branch}:${path}") {
+            ... on Blob {
+              text
+              oid
+            }
+          }
+        `)
+    });
+
+    arr.push(`
+        }
+    }
+    `)
+
+    const query = arr.join()
+
+    const variables = {
+        "owner": owner,
+        "name": name,
+    }
+
+    return { query, variables }
+}
 
 const processQuery = async (token: string, body: any) => {
 
@@ -80,39 +118,70 @@ const processQuery = async (token: string, body: any) => {
 
         if (!response.ok) {
             console.log(response)
-            return []
+            return resultError<any>('Response is not 200!')
         }
 
         const json = await response.json()
         console.log('processQuery:', json)
-        return json
+        return resultSuccess<any>(json)
 
     } catch (e) {
         console.log("Error", e)
+        return resultError<any>(`Error: ${e} `)
     }
 }
 
+// TODO: Other file - separate grqaphql from files api
 export const gqlGetFile = async (repoConfig: TRepoConfig, path: string) => {
 
     try {
 
         const body = getFileQuery(repoConfig.owner, repoConfig.name, 'main', path)
-        const json = await processQuery(repoConfig.token, body)
-        return json.data.repository.object as TFile
+        const result = await processQuery(repoConfig.token, body)
+        if (result.hasError()) {
+            return resultError<TFile>(result.getError())
+        }
+        return resultSuccess<TFile>(result.getValue().data.repository.object as TFile)
     } catch (e) {
         console.log("Error", e)
+        return resultError<TFile>(`Error: ${e} `)
+    }
+}
+
+// TODO: Order ? Return map with path as key?
+export const gqlGetFiles = async (repoConfig: TRepoConfig, paths: string[]) => {
+
+    try {
+
+        const body = getFilesQuery(repoConfig.owner, repoConfig.name, 'main', paths)
+        const result = await processQuery(repoConfig.token, body)
+        if (result.hasError()) {
+            return resultError<TFile[]>(result.getError())
+        }
+
+        const files: TFile[] = []
+
+        for (const value of Object.values(result.getValue().data.repository)) {
+            files.push(value as TFile)
+        }
+
+        return resultSuccess<TFile[]>(files)
+    } catch (e) {
+        console.log("Error", e)
+        return resultError<TFile>(`Error: ${e} `)
     }
 }
 
 export const gqlGetListing = async (repoConfig: TRepoConfig, path: string) => {
 
     try {
-
         const body = getListingQuery(repoConfig.owner, repoConfig.name, 'main', path)
-        const json = await processQuery(repoConfig.token, body)
-        return json.data.repository.object.entries as TListing[]
+        const result = await processQuery(repoConfig.token, body)
+        if (result.hasError()) {
+            return resultError<TListing[]>(result.getError())
+        }
+        return resultSuccess<TListing[]>(result.getValue().data.repository.object.entries as TListing[])
     } catch (e) {
-        console.log("Error", e)
-        return []
+        return resultError<TListing[]>(`Error: ${e} `)
     }
 }
