35d4f47e6a76832885fdb95ff59d75780f7ba080
Dead End <dead.end@inbox.ru>
2024-03-02 10:21:05 +0100

    changed error handling

--- src/ts/github/github.ts
+++ src/ts/github/github.ts
@@ -1,41 +1,29 @@
-import Result from '../libs/result'
-
 const GQL_URL = 'https://api.github.com/graphql'
 
 /**
  * The function processes a github graphql query.
  */
-export const processQuery = async (token: string, body: any) => {
-    const res = new Result<any>()
-    try {
-        const data = {
-            method: 'POST',
-            headers: {
-                authorization: `bearer ${token}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        }
-
-        const response = await fetch(GQL_URL, data)
-
-        if (!response.ok) {
-            return res.failed(
-                `Response: ${response.status} - ${response.statusText}`
-            )
-        }
-
-        const json = await response.json()
-        if (json.errors) {
-            console.log(json)
-            const errors = json.errors.map(
-                (e: { message: string }) => e.message
-            )
-            return res.failed(errors.join(', '))
-        }
-
-        return res.success(json)
-    } catch (e) {
-        return res.failed(`Unable to process query: ${e} `)
+export const processGithubQuery = async (token: string, body: any) => {
+    const data = {
+        method: 'POST',
+        headers: {
+            authorization: `bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
     }
+
+    const response = await fetch(GQL_URL, data)
+
+    if (!response.ok) {
+        throw new Error(`Response: ${response.status} - ${response.statusText}`)
+    }
+
+    const json = await response.json()
+    if (json.errors) {
+        const errors = json.errors.map((e: { message: string }) => e.message)
+        throw new Error(errors.join(', '))
+    }
+
+    return json
 }
