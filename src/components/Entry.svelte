b7105b08c37ce01abbb54d1339ad70d3d13bc318
Dead End <dead.end@inbox.ru>
2024-03-10 12:16:07 +0100

    Added entry

---
+++ src/components/Entry.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { TEntry } from '../ts/types'
+
+    export let entry: TEntry
+    export let commit: string
+    export let show: boolean
+</script>
+
+{#if show && entry}
+    <div class="text-sm text-right text-gray-500 pb-4">
+        <div>
+            Modifield: {new Date(entry.tc_modified).toLocaleString()}
+        </div>
+        <div>
+            Commit: {commit.substring(0, 7)}
+        </div>
+    </div>
+
+    <h3 class="pb-4">ID: {entry.tc_id}</h3>
+{/if}
