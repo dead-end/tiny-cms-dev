73e526b836d74eb0e4f6043190b450210770a953
Dead End <dead.end@inbox.ru>
2024-03-23 11:18:54 +0100

    removed class

--- src/components/icons/ListIcon.svelte
+++ src/components/icons/ListIcon.svelte
@@ -2,7 +2,7 @@
     export let onClick: () => void
 </script>
 
-<button class="" on:click={onClick}>
+<button on:click={onClick}>
     <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
